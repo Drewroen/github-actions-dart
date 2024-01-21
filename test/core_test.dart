ce8d8e352b2dae7c612efb3f3286770b17d9247c
drewroen <drewroen@gmail.com>
2024-01-21 15:11:07 -0600

    Organize groups

--- test/core_test.dart
+++ test/core_test.dart
@@ -4,7 +4,7 @@ import 'package:core/core.dart' as core;
 import 'fakes/fake_ioSink.dart';
 
 void main() {
-  group('Empty tests', () {
+  group('Core tests:', () {
     late PrintService printService;
     late FakeIOSink fakeIOSink;
     setUp(() {
@@ -13,61 +13,95 @@ void main() {
       core.injectPrintService(printService);
     });
 
-    test('Sends an info message', () {
-      core.info("Hello");
-      expect(fakeIOSink.outputs.length, 1);
-      expect(fakeIOSink.outputs[0], "Hello");
+    group('core.info', () {
+      test('Sends a message', () {
+        core.info("Hello");
+        expect(fakeIOSink.outputs.length, 1);
+        expect(fakeIOSink.outputs[0], "Hello");
+      });
     });
 
-    test('Sends a debug message', () {
-      core.debug("Hello");
-      expect(fakeIOSink.outputs.length, 1);
-      expect(fakeIOSink.outputs[0], "::debug::Hello");
+    group('core.debug', () {
+      test('Sends a message', () {
+        core.debug("Hello");
+        expect(fakeIOSink.outputs.length, 1);
+        expect(fakeIOSink.outputs[0], "::debug::Hello");
+      });
     });
 
-    test('Sends a notice message', () {
-      core.notice("Hello");
-      expect(fakeIOSink.outputs.length, 1);
-      expect(fakeIOSink.outputs[0], "::notice::Hello");
+    group('core.notice', () {
+      test('Sends a message', () {
+        core.notice("Hello");
+        expect(fakeIOSink.outputs.length, 1);
+        expect(fakeIOSink.outputs[0], "::notice::Hello");
+      });
     });
 
-    test('Sends a warning message', () {
-      core.warning("Hello");
-      expect(fakeIOSink.outputs.length, 1);
-      expect(fakeIOSink.outputs[0], "::warning::Hello");
+    group('core.warning', () {
+      test('Sends a message', () {
+        core.warning("Hello");
+        expect(fakeIOSink.outputs.length, 1);
+        expect(fakeIOSink.outputs[0], "::warning::Hello");
+      });
     });
 
-    test('Sends an error message', () {
-      core.error("Hello");
-      expect(fakeIOSink.outputs.length, 1);
-      expect(fakeIOSink.outputs[0], "::error::Hello");
+    group('core.error', () {
+      test('Sends a message', () {
+        core.error("Hello");
+        expect(fakeIOSink.outputs.length, 1);
+        expect(fakeIOSink.outputs[0], "::error::Hello");
+      });
+
+      test('Sends a message with annotation properties', () {
+        core.error("Hello",
+            properties: core.AnnotationProperties(
+                file: "file.txt",
+                col: 1,
+                endColumn: 20,
+                line: 1,
+                endLine: 2,
+                title: "MY TITLE"));
+        expect(fakeIOSink.outputs.length, 1);
+        expect(fakeIOSink.outputs[0],
+            "::error title=MY TITLE,file=file.txt,col=1,endColumn=20,line=1,endLine=2::Hello");
+      });
     });
 
-    test('Sends an error message with annotation properties', () {
-      core.error("Hello",
-          properties: core.AnnotationProperties(file: "file.txt", line: 1));
-      expect(fakeIOSink.outputs.length, 1);
-      expect(fakeIOSink.outputs[0], "::error file=file.txt,line=1::Hello");
+    group('core.startGroup', () {
+      test('Starts a group', () {
+        core.startGroup("Hello");
+        expect(fakeIOSink.outputs.length, 1);
+        expect(fakeIOSink.outputs[0], "::group::Hello");
+      });
     });
 
-    test('Sends a message that starts a group', () {
-      core.startGroup("Hello");
-      expect(fakeIOSink.outputs.length, 1);
-      expect(fakeIOSink.outputs[0], "::group::Hello");
+    group('core.endGroup', () {
+      test('Ends a group', () {
+        core.endGroup();
+        expect(fakeIOSink.outputs.length, 1);
+        expect(fakeIOSink.outputs[0], "::endgroup::");
+      });
     });
 
-    test('Sends a message that ends a group', () {
-      core.endGroup();
-      expect(fakeIOSink.outputs.length, 1);
-      expect(fakeIOSink.outputs[0], "::endgroup::");
-    });
+    group('core.group', () {
+      test('Starts a group and then ends a group', () {
+        core.group("Hello", () {
+          return;
+        });
+        expect(fakeIOSink.outputs.length, 2);
+        expect(fakeIOSink.outputs[0], "::group::Hello");
+        expect(fakeIOSink.outputs[1], "::endgroup::");
+      });
 
-    test('Sends a message within a group', () {
-      core.group("Hello", () => {core.info("Hello2")});
-      expect(fakeIOSink.outputs.length, 3);
-      expect(fakeIOSink.outputs[0], "::group::Hello");
-      expect(fakeIOSink.outputs[1], "Hello2");
-      expect(fakeIOSink.outputs[2], "::endgroup::");
+      test('Starts a group, runs a function and then ends a group', () {
+        core.group("Hello", () {
+          core.info("This is my inside function");
+        });
+        expect(fakeIOSink.outputs.length, 3);
+        expect(fakeIOSink.outputs[0], "::group::Hello");
+        expect(fakeIOSink.outputs[1], "This is my inside function");
+        expect(fakeIOSink.outputs[2], "::endgroup::");
+      });
     });
   });
 }
